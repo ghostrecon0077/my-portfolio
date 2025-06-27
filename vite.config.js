2301107c4a782898d81627619f2f776604a3a57f
ghostrecon0077 <shahawais35@gmail.com>
2025-06-27 10:00:46 +0500

    Added automated deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/my-portfolio-website/', // Replace with your GitHub repository name
+  base: '/my-portfolio-website/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
   }
 })
